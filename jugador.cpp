c0ffecab24770c15229b05804fd8b799a1a716a8
Stephany18D <111327459+Stephany18D@users.noreply.github.com>
2024-05-21 22:06:56 -0500

    lab-5

---
+++ jugador.cpp
@@ -0,0 +1,7 @@
+#include "jugador.h"
+
+Jugador::Jugador()
+{
+    setPixmap(QPixmap(":/images/player.png"));
+    setScale(0.1);
+}
---
+++ jugador.h
@@ -0,0 +1,12 @@
+#ifndef JUGADOR_H
+#define JUGADOR_H
+
+#include <QGraphicsPixmapItem>
+
+class Jugador : public QGraphicsPixmapItem
+{
+public:
+    Jugador();
+};
+
+#endif // JUGADOR_H
